cd1d700aadc1ba8ddad73dad3c157e4ab597f23e
Patrick Sheehan <psheehan0301@gmail.com>
2014-11-20 19:57:04 -0600

    Main method with test code

--- scripts/content-watchdog.py
+++ scripts/content-watchdog.py
@@ -14,8 +14,7 @@ class ContentWatchdog:
     # blockPhoneNumbers = True
     # blockEmailAddress = True
 
-    @staticmethod
-    def shouldBeSubmitted(self, message):
+    def shouldSubmitMessage(self, message):
         if len(message) < self.minLength or len(message) > self.maxLength:
             return False
         if self.blockEmail(message):
@@ -30,21 +29,43 @@ class ContentWatchdog:
 
     def blockEmail(self, message):
         if self.blockEmailAddress:
-            return re.match("\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b", message)
+            return re.search("\b[A-Z0-9._%+-]\*+@[A-Z0-9.-]+\.[A-Z]{2,4}\b", message)
 
         return False
     # check if message should be blocked because of an email address
 
     def blockNumber(self, message):
+        # if self.blockEmailAddress:
+        #     return re.search("[0-9]{3}")
+
         return False
+
     # check if message should be blocked because of a phone number
 
     def isVulgar(self, message):
         return False
     # check if the message is classified as vulgar
 
-    def isSexual:
+    def isSexual(self, message):
         return False
     # check if the message is classified as sexual
 
 
+if __name__ == '__main__':
+
+    watchdog = ContentWatchdog(10, 140, True, True)
+    messages = ['howdy fools, what\'s up with yall?',
+                'yo yo yo test post #1642',
+                'this has an email@address.com',
+                'girl can I have yo numba? can I have it?',
+                'ya try 911 fool',
+                'its actually 348-555-4718',
+                'Big Poppa\'s number be da following: (124) 555- 6843']
+
+    for message in messages:
+        result = 'Accepted: ' if watchdog.shouldSubmitMessage(message) else 'REJECTED: '
+        print result + message
+
+    pass
+
+    #your code
