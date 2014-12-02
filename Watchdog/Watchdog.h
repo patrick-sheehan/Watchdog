c5a23aa99dee1d288f778bd3bc584145517e80dd
Patrick Sheehan <psheehan0301@gmail.com>
2014-12-02 13:34:17 -0600

    Added empty Xcode project

---
+++ Watchdog/Watchdog.h
@@ -0,0 +1,13 @@
+//
+//  Watchdog.h
+//  Watchdog
+//
+//  Created by Patrick Sheehan on 12/2/14.
+//  Copyright (c) 2014 Appuccino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Watchdog : NSObject
+
+@end
---
+++ Watchdog/Watchdog.m
@@ -0,0 +1,13 @@
+//
+//  Watchdog.m
+//  Watchdog
+//
+//  Created by Patrick Sheehan on 12/2/14.
+//  Copyright (c) 2014 Appuccino. All rights reserved.
+//
+
+#import "Watchdog.h"
+
+@implementation Watchdog
+
+@end
